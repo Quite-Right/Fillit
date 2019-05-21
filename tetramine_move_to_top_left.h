b6b48d8890019443008ff5a28955848c94dcc69f
Morozov Ivan <john117.morozov@yandex.ru>
2019-05-21 20:22:31 +0400

    Добавлены функции сдвига тетрамины в левый верхний угол

---
+++ tetramine_move_to_top_left.h
@@ -0,0 +1,8 @@
+
+#ifndef FILLIT_TETRAMINE_MOVE_TO_TOP_LEFT_H
+#define FILLIT_TETRAMINE_MOVE_TO_TOP_LEFT_H
+
+//Сдвиг тетрамины вверх влево
+void tetramine_move_to_top_left(char **tetr);
+
+#endif //FILLIT_TETRAMINE_MOVE_TO_TOP_LEFT_H
